a46137af19818518fae00ba0b73ee76a872bfb84
lucChesneau <luc.chesneau41@gmail.com>
2023-07-13 11:53:02 +0200

    tp5 finished. Favoris gardés en mémoire et redispatchés sur la deuxième activité

--- app/src/main/java/com/luc/android/cdafirstapp/activities/ScrollingActivity.java
+++ app/src/main/java/com/luc/android/cdafirstapp/activities/ScrollingActivity.java
@@ -2,6 +2,7 @@ package com.luc.android.cdafirstapp.activities;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -21,12 +22,17 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.luc.android.cdafirstapp.R;
+import com.luc.android.cdafirstapp.Util;
 import com.luc.android.cdafirstapp.adapters.CityAdapter;
 import com.luc.android.cdafirstapp.databinding.ActivityScrollingBinding;
 import com.luc.android.cdafirstapp.models.City;
 import com.luc.android.cdafirstapp.models.weather_api.CityRetrofit;
 import com.luc.android.cdafirstapp.network.ApiService;
 import com.luc.android.cdafirstapp.network.RetrofitClient;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -48,8 +54,13 @@ public class ScrollingActivity extends AppCompatActivity {
         binding = ActivityScrollingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mRecyclerView = findViewById(R.id.cities_recycler_view);
+        new RetrofitClient();
+        ApiService apiService = RetrofitClient.getInstance().getApi();
 
+
+        ArrayList<String> citiesDeFou = getFavoriteCities();
+
+        mRecyclerView = findViewById(R.id.cities_recycler_view);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(layoutManager);
 
@@ -57,13 +68,45 @@ public class ScrollingActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(mAdapter);
 
 
+        for(int i = 0; i < citiesDeFou.size(); i++){
+            Call<CityRetrofit> calll = apiService.getResponseByCityName(citiesDeFou.get(i), "fr", "metric","01897e497239c8aff78d9b8538fb24ea");
+            calll.enqueue(new retrofit2.Callback<CityRetrofit>() {
+                @Override
+                public void onResponse(Call<CityRetrofit> call, retrofit2.Response<CityRetrofit> response) {
+                    if (response.isSuccessful()) {
+                        CityRetrofit cityRetrofit = response.body();
+
+                        mCities.add(cityRetrofit);
+
+                        Util.saveFavouriteCities(ScrollingActivity.this, mCities);
+
+                        mAdapter.notifyDataSetChanged();
+
+                    } else {
+                        Toast.makeText(ScrollingActivity.this, "Error: " + response.code(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<CityRetrofit> call, Throwable t) {
+                    Log.d("testdefou", "Erreur lors de l'envoi de la requête : " + t.getMessage());
+                    t.printStackTrace();
+                }
+            });
+        }
+
+        mAdapter.notifyDataSetChanged();
+        //Util.initFavoriteCities(this);
+        //getFavoriteCities();
+
+
+
         Toolbar toolbar = binding.toolbar;
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
         toolBarLayout.setTitle(getTitle());
 
-        new RetrofitClient();
-        ApiService apiService = RetrofitClient.getInstance().getApi();
+
 
 
 
@@ -92,7 +135,10 @@ public class ScrollingActivity extends AppCompatActivity {
 
                                 mCities.add(cityRetrofit);
 
-                                Toast.makeText(ScrollingActivity.this, "Data received!", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(ScrollingActivity.this, "Data received! and city saved", Toast.LENGTH_SHORT).show();
+                                Util.saveFavouriteCities(ScrollingActivity.this, mCities);
+                                //getFavoriteCities();
+
                                 mAdapter.notifyDataSetChanged();
 
                             } else {
@@ -113,4 +159,25 @@ public class ScrollingActivity extends AppCompatActivity {
             }
         });
     }
+
+    public ArrayList<String> getFavoriteCities() {
+        SharedPreferences preferences = ScrollingActivity.this.getSharedPreferences(Util.PREFS_NAME,
+                Context.MODE_PRIVATE);
+        ArrayList<String> cities = new ArrayList<>();
+        try {
+            JSONArray jsonArray = new JSONArray(preferences.getString(Util.PREFS_FAVORITE_CITIES, "[]"));
+            for (int i = 0; i < jsonArray.length(); i++) {
+                String cityName = jsonArray.getString(i);
+                cities.add(cityName);
+                Log.d("megatest", cityName);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return cities;
+    }
+
+
+
+
 }
